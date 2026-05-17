3b07d0b25ab51a36cd0b07304022cedce979d394
arifnotify <arifnotify@gmail.com>
2026-05-17 12:31:37 +0300

    update 4

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { Product, ProductSchema } from './schemas/product.schema';
+
+import { AuthGuard } from '../auth/auth.guard';
+import { RolesGuard } from '../roles/roles.guard';
 
 @Module({
   imports: [
@@ -13,8 +18,14 @@ import { Product, ProductSchema } from './schemas/product.schema';
         schema: ProductSchema,
       },
     ]),
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secret',
+    }),
   ],
+
   controllers: [ProductsController],
-  providers: [ProductsService],
+
+  providers: [ProductsService, AuthGuard, RolesGuard],
 })
 export class ProductsModule {}
