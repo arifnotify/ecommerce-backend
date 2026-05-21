d20be64cde84318089bd6a62a9b27e73e6682207
arifnotify <arifnotify@gmail.com>
2026-05-21 13:37:42 +0300

    update 23

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -1,9 +1,16 @@
+// src/config/cloudinary.config.ts
+
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Debug (optional)
+console.log('Cloudinary Connected:', {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 });
 
 export default cloudinary;
