bd1a0fad3205421615d4d991fc7397000276a529
arifnotify <arifnotify@gmail.com>
2026-05-22 12:47:12 +0300

    update 3

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,6 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 import { User, UserSchema } from '../users/schemas/user.schema';
-import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
@@ -19,6 +18,5 @@ import { AuthGuard } from './auth.guard';
   ],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [AuthGuard, JwtModule],
 })
 export class AuthModule {}
