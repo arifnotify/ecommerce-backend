3e0ea0993ba55cbe252268410496a1dc0157b2e8
arifnotify <arifnotify@gmail.com>
2026-05-22 22:53:44 +0300

    update

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -16,23 +16,25 @@ import { AuthGuard } from '../auth/auth.guard';
 export class OrdersController {
   constructor(private ordersService: OrdersService) {}
 
-  // Create Order (Protected)
+  // Create Order
   @UseGuards(AuthGuard)
   @Post()
-  create(@Req() req: any, @Body() body: any) {
-    return this.ordersService.create(req.user.id, body);
+  async create(@Req() req: any, @Body() body: any) {
+    return await this.ordersService.create(req.user.id, body);
   }
 
-  // Get All Orders (Public)
+  // Show Only Orders Data
   @Get()
-  findAll() {
-    return this.ordersService.findAll();
+  async findAll() {
+    const orders = await this.ordersService.findAll();
+
+    return orders;
   }
 
-  // Update Order Status (Protected)
+  // Update Order Status
   @UseGuards(AuthGuard)
   @Patch(':id')
-  updateStatus(@Param('id') id: string, @Body() body: any) {
-    return this.ordersService.updateStatus(id, body.status);
+  async updateStatus(@Param('id') id: string, @Body() body: any) {
+    return await this.ordersService.updateStatus(id, body.status);
   }
 }
