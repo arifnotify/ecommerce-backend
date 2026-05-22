7a21ab70d4b63a0ac2ea7fe69f24c7d5c59cdda0
arifnotify <arifnotify@gmail.com>
2026-05-22 19:36:27 +0300

    update 5

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,35 +1,13 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Get, Param, Patch, Post, Req } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private ordersService: OrdersService) {}
 
-  @UseGuards(AuthGuard('jwt'))
   @Post()
-  async create(@Req() req: any, @Body() body: any) {
-    try {
-      console.log('USER =>', req.user);
-      console.log('BODY =>', body);
-
-      return await this.ordersService.create(req.user.id, body);
-    } catch (error) {
-      console.log(error);
-
-      return {
-        error: error.message,
-      };
-    }
+  create(@Req() req: any, @Body() body: any) {
+    return this.ordersService.create(req.user.id, body);
   }
 
   @Get()
